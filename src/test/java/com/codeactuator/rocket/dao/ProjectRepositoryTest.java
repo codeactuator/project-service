6e20e984b508fb5ae802e6be44cd2cc9aeca7fa9
Shekhar Kumar <shekharkumargupta@gmail.com>
2020-08-26 02:30:19 +0530

    replace assert to assertThat

--- src/test/java/com/codeactuator/rocket/dao/ProjectRepositoryTest.java
+++ src/test/java/com/codeactuator/rocket/dao/ProjectRepositoryTest.java
@@ -3,14 +3,14 @@ package com.codeactuator.rocket.dao;
 import com.codeactuator.rocket.domain.Project;
 import com.codeactuator.rocket.domain.Task;
 import com.codeactuator.rocket.dto.ProjectDTO;
-import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -22,19 +22,14 @@ public class ProjectRepositoryTest {
     @Autowired
     private ProjectRepository projectRepository;
 
-    @Autowired
-    private WorkforceRepository workforceRepository;
-
-    @Autowired
-    private TaskTypeRepository taskTypeRepository;
-
 
     @Test
     public void should_create_project(){
         Project atlantis = createProject("ATLANTIS");
-        assert(atlantis != null);
-        assert (atlantis.getId() > 0);
-        Assert.assertThat(atlantis, Matchers.isA(Project.class));
+
+        assertThat(atlantis).isNotNull();
+        assertThat(atlantis.getId()).isGreaterThan(0);
+        assertThat(atlantis).isInstanceOf(Project.class);
     }
 
     @Test
@@ -44,16 +39,16 @@ public class ProjectRepositoryTest {
         Task task = new Task();
         task.setName("Analysis and Documentation");
         blue_jeans.addTask(task);
-
         blue_jeans = projectRepository.save(blue_jeans);
 
         ProjectDTO projectDTO = new ProjectDTO();
         projectDTO.unmarshal(blue_jeans);
         System.out.println(projectDTO);
 
-        assert(blue_jeans != null);
-        assert(blue_jeans.getTasks() != null);
-        assert (blue_jeans.getTasks().size() > 0);
+
+        assertThat(blue_jeans).isNotNull();
+        assertThat(blue_jeans.getId()).isGreaterThan(0);
+        assertThat(blue_jeans.getTasks()).isNotEmpty();
     }
 
     private Project createProject(String projectName){
--- src/test/java/com/codeactuator/rocket/service/ProjectServiceImplTest.java
+++ src/test/java/com/codeactuator/rocket/service/ProjectServiceImplTest.java
@@ -1,5 +1,55 @@
 package com.codeactuator.rocket.service;
 
 
+import com.codeactuator.rocket.dto.ProjectDTO;
+import com.codeactuator.rocket.dto.TaskDTO;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
 public class ProjectServiceImplTest {
+
+
+
+
+
+    public void create(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void remove(){
+
+    }
+
+    public void removeById(){
+
+    }
+
+    public void findById(){
+
+    }
+
+    public void findAll(){
+
+    }
+
+    public void findAllByOrganizationId(){
+
+    }
+
+    public void resources(){
+
+    }
+
+    public void tasks(){
+
+    }
+
 }
