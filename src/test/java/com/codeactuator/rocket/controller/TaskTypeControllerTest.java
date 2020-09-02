7ec80b81641cf3dcf81d4ade4d03e1c3e2db5c6c
Shekhar Kumar <shekharkumargupta@gmail.com>
2020-09-02 13:33:53 +0530

    Added JUnit 5 Jupiter dependency and TaskTypeControllerTest

--- src/test/java/com/codeactuator/rocket/controller/TaskTypeControllerTest.java
+++ src/test/java/com/codeactuator/rocket/controller/TaskTypeControllerTest.java
@@ -2,7 +2,6 @@ package com.codeactuator.rocket.controller;
 
 
 import com.codeactuator.rocket.config.ConfigProperties;
-import com.codeactuator.rocket.dto.TaskTypeDTO;
 import com.codeactuator.rocket.service.TaskTypeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -10,9 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = {TaskTypeController.class})
 public class TaskTypeControllerTest {
@@ -31,13 +30,7 @@ public class TaskTypeControllerTest {
 
     @Test
     public void whenListen_thenReturns200() throws Exception {
-        TaskTypeDTO taskTypeDTO = new TaskTypeDTO
-                .Builder("Development")
-                .build();
-
-        mockMvc.perform(
-                MockMvcRequestBuilders.get("/v1/taskTypes/ping"))
-                .andExpect(MockMvcResultMatchers.request().asyncNotStarted())
-                .andReturn();
+        mockMvc.perform(get("/v1/taskTypes/ping"))
+                .andExpect(status().isOk());
     }
 }
