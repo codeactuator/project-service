081fa64d29759ca6637d29d614dadc01cf842207
Shekhar Kumar <shekharkumargupta@gmail.com>
2020-10-19 14:39:31 +0530

    Removed unused import

--- src/main/java/com/codeactuator/rocket/dao/TaskTypeRepository.java
+++ src/main/java/com/codeactuator/rocket/dao/TaskTypeRepository.java
@@ -1,7 +1,6 @@
 package com.codeactuator.rocket.dao;
 
 import com.codeactuator.rocket.domain.TaskType;
-import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
